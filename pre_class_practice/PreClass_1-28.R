00263e4e6e40fde098a8dffba84f8fd5c490ba82
Myles Ashbaker <mylesashbaker@dhcp-10-5-70-129.ad.uvu.edu>
2025-01-28 11:16:39 -0700

    update

---
+++ pre_class_practice/PreClass_1-28.R
@@ -0,0 +1,22 @@
+mtcars
+
+##1- what type of object is this?
+str(mtcars)
+class(mtcars)
+ = 'data.frame'
+typeof(mtcars)
+ = 'list'
+
+##2- find 4 cylinders cars with a mpg greater than 20, and save them to a new object
+cyl4_mpg20_cars <- mtcars[mtcars$cyl == 4 & mtcars$mpg > 20, ]
+cyl4_mpg20_cars
+
+##3- convert mpg to a character data type
+mtcars$mpg <- as.character(mtcars$mpg)
+class(mtcars$mpg)
+
+##4- convert the entire data frame to character data type
+mtcars_character <- data.frame(lapply(mtcars, as.character))
+mtcars_character
+str(mtcars_character)
+

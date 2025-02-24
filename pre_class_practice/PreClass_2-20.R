b2c2618d83c9f56855091da0a40f639540da5173
Myles Ashbaker <mylesashbaker@MacBookPro.sumo.io>
2025-02-24 15:06:22 -0700

    Exam 1

---
+++ pre_class_practice/PreClass_2-20.R
@@ -0,0 +1,13 @@
+library(gapminder)
+library(ggimage)
+library(gganimate)
+library(patchwork)
+library(ggmap)
+library(tidyverse)
+
+location <- "new york city"
+
+df <- read.csv('Data/wide_income_rent.csv')
+
+View(df)
+
